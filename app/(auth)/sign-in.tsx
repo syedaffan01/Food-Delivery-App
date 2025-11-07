9acb83c14e0ac9ee9bb20213ef9ca17e6750d73c
syedaffan01 <affan4157@gmail.com>
2025-11-08 03:35:37 +0530

    Created Auth screens(signin and signup)

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
-import {router} from "expo-router";
+import React, {useState} from 'react';
+import {View, Text, Button, Alert} from 'react-native';
+import {Link, router} from "expo-router";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
 
 const SignIn = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [form, setForm] = useState({email:'', password:''});
+
+    const submit = async () => {
+        if(!form.email || !form.password) Alert.alert('Error', 'Please enter valid email address & password');
+
+
+    setIsSubmitting(true);
+
+    try{
+//Call appwrite sign in func
+        Alert.alert('Success', 'User signed in successfully. ');
+        router.replace('/')
+    } catch (error: any){
+        Alert.alert('Error', error.message);
+    } finally{
+        setIsSubmitting(false);
+    }
+}
+
   return (
-    <View>
-      <Text>SignIn</Text>
-        <Button title="Sign Up" onPress={() => router.push("/sign-up")} />
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+        <CustomInput
+            placeholder="Enter your email"
+            value={form.email}
+            onChangeText={(text) => setForm((prevState) =>({...prevState, email: text}) )}
+            label="Email"
+            keyboardType="email-address"
+
+        />
+        <CustomInput
+            placeholder="Enter your password"
+            value={form.password}
+            onChangeText={(text) => setForm((prevState) =>({...prevState, password: text}) )}
+            label="Password"
+            secureTextEntry={true}
+
+        />
+        <CustomButton
+       title="Sign In"
+       isLoading={isSubmitting}
+       onPress={submit}
+
+
+        />
+        <View className="flex justify-center mt-5 flex-row gap-2">
+            <Text className="base-regular text-gray-100">
+                Don't have an account?
+            </Text>
+            <Link href="/sign-up" className="base-bold text-primary">
+                 Sign Up
+            </Link>
+        </View>
+
+
     </View>
   );
 };
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.aff.foodordering",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '690d258700016a5019fa',
+    userCollectionId: 'user'
+}
